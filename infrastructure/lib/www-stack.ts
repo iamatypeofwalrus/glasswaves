56e531938f080a07e2eeb03d7b6aa93a2aa8923a
Joe Feeney <joe@glasswaves.co>
2018-12-17 22:54:40 -0800

    add CloudFront distribution for WWW

--- infrastructure/lib/www-stack.ts
+++ infrastructure/lib/www-stack.ts
@@ -1,5 +1,24 @@
 import cdk = require('@aws-cdk/cdk');
 import s3 = require('@aws-cdk/aws-s3');
+import cloudfront = require('@aws-cdk/aws-cloudfront');
+
+// TODO:
+// * accept root domain name e.g. glasswaves.co
+// * an ACM certificate arn
+// * HostedZoneId to create attach to a Route 53 record set like
+//  RootRecordSet:
+//  Type: "AWS::Route53::RecordSet"
+//  Properties:
+//  Type: "A"
+//  Name: glasswaves.co.
+//    HostedZoneId:
+//  Fn:: ImportValue: glasswaves - co - hostedzone
+//  AliasTarget:
+//  HostedZoneId: Z2FDTNDATAQYW2
+//  DNSName:
+//  Fn:: GetAtt:
+//  - RootCloudfrontDistribution
+//    - DomainName
 
 export class WwwStack extends cdk.Stack {
   constructor(parent: cdk.App, name: string, props?: cdk.StackProps) {
@@ -22,5 +41,37 @@ export class WwwStack extends cdk.Stack {
     };
 
     www.export();
+
+    new cloudfront.CloudFrontWebDistribution(this, 'WwwDistribution', {
+      comment: "Distribution pointing to the www bucket",
+      priceClass: cloudfront.PriceClass.PriceClassAll,
+      errorConfigurations: [
+        {
+          errorCode: 404,
+          responseCode: 404,
+          responsePagePath: "/404.html",
+          errorCachingMinTtl: 30
+        },
+        {
+          errorCode: 403,
+          responseCode: 404,
+          responsePagePath: "/404.html",
+          errorCachingMinTtl: 30
+        }
+      ],
+      originConfigs: [
+        {
+          s3OriginSource: {
+            s3BucketSource: www
+          },
+          behaviors: [
+            {
+              compress: true,
+              isDefaultBehavior: true
+            }
+          ]
+        }
+      ]
+    });
   }
 }
